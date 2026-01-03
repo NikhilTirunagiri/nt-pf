bede002983e6a8ab8f1b5c4c827b7ceed2d5226d
NikhilTirunagiri <nikhiltirunagiri@outlook.com>
2026-01-03 14:03:28 -0500

    clickable links on the sidebar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
 "use client"
 
-
-
 import Sidebar from "@/components/Sidebar";
+
 
 export default function Home(){
     return(
@@ -12,7 +11,23 @@ export default function Home(){
             </div>
             <div className="w-full flex flex-col">
                 <div className="justify-items-end text-m font-medium" ><p>📌 Fairfax, VA; 12:49</p></div>
-                <div> <h1> This is the home page</h1></div>
+                <div className="gap-2 flex flex-col"> 
+                    <span>
+                        Welcome to my website :) <br/>
+                    </span>
+                    <span>
+                        You can find a number of things on the site: <br/>
+                    </span>
+                    <span className="font-bold">
+                        <span className="underline">projects im working on</span> 
+                        <span>, </span>
+                        <span className="underline">blogs</span> 
+                        <span>, </span>
+                        <span className="underline">and my contact info</span>
+                        <span></span>
+                        </span>
+                
+                </div>
             </div>
         </div>
     );
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,20 +1,42 @@
-"useclient"
+"use client"
+import { usePathname } from "next/navigation";
+import { useRouter } from "next/router";
+import { rotate } from "three/tsl";
+
+
+export function useCurrentPath(){
+    return usePathname()
+}
+
+export function pushToPage({children, href}){
+    const router = useRouter()
+    
+    const handleClick = (e) => {
+        e.preventDefault()
+        router.push(href)
+    }
+}
+
+
+
 
 export default function Sidebar(){
+    const pathname = useCurrentPath()
+    
     return(
         <div className="flex flex-col pr-10">  
             <div className="pb-5">
-                <h1 className="font-bold">nikhilt.dev</h1>
+                <a className="font-bold" href="https://www.nikhilt.dev/">nikhilt.dev</a>
             </div>
             <div className="font-medium w-40">
                 <ul>
-                    <li className="hover:bg-red-200"><a href="/">Home</a></li>
-                    <li className="hover:bg-red-200"><a href="/about">About</a></li>
-                    <li className="hover:bg-red-200"><a href="/blogs">Blogs</a></li>
-                    <li className="hover:bg-red-200"><a href="/projexts">Projects</a></li>
-                    <li className="hover:bg-red-200"><a href="/archive">Archive</a></li>
-                    <li className="hover:bg-red-200"><a href="/tags">Tags</a></li>
-                    <li className="hover:bg-indigo-200"><a href="/contact">Contact</a></li>    
+                    <li className={pathname === "/" ? "bg-red-300" : "hover:bg-red-300 onClick={handleClick}"}><a href="/">Home</a></li>
+                    <li className={pathname === "/about" ? "bg-red-300" : "hover:bg-red-200 onClick={handleClick}"}><a href="/about">About</a></li>
+                    <li className={pathname === "/blogs" ? "bg-red-300" : "hover:bg-red-200 onClick={handleClick}"}><a href="/blogs">Blogs</a></li>
+                    <li className={pathname === "/projexts" ? "bg-red-300" : "hover:bg-red-200 onClick={handleClick}"}><a href="/projexts">Projects</a></li>
+                    <li className={pathname === "/archive" ? "bg-red-300" : "hover:bg-red-200 onClick={handleClick}"}><a href="/archive">Archive</a></li>
+                    <li className={pathname === "/tags" ? "bg-red-300" : "hover:bg-red-200 onClick={handleClick}"}><a href="/tags">Tags</a></li>
+                    <li className={pathname === "/contact" ? "bg-blue-300" : "hover:bg-blue-200 onClick={handleClick}"}><a href="/contact">links</a></li>    
                 </ul>
             </div>
         </div>
