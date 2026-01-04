038d5267850883e4dc18af3fc6ed02881794f916
NikhilTirunagiri <nikhiltirunagiri@outlook.com>
2026-01-04 15:47:58 -0500

    archive content

--- src/app/archive/page.tsx
+++ src/app/archive/page.tsx
@@ -6,8 +6,13 @@ import Topbar from "@/components/Topbar";
 
 export default function Home(){
     return(
-        <span className="">
-            You can view the old versions of this site here:<br/>
-        </span>
+        <div>
+            <span>
+                You can view the old versions of this site here:<br/>
+            </span>
+            <ul>
+                <li><a href="https://www.nikhilt.dev/mac" className="underline text-blue-600">v1 - macintosh style</a></li>
+            </ul>
+        </div>
     );
 }
