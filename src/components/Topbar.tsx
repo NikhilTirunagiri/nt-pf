c87faca2a6cad027cf7ba956f7310a70eb24ae2e
NikhilTirunagiri <nikhiltirunagiri@outlook.com>
2026-01-03 23:57:17 -0500

    topbar component

---
+++ src/components/Topbar.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+
+export default function Topbar(){
+    return(
+            <div className="w-full flex flex-col">
+                <div className="justify-items-end text-13px font-normal" >
+                    <p>📍 Fairfax, VA</p></div>
+                <div className="gap-2 flex flex-col"> 
+                </div>
+            </div>
+    );
+}
