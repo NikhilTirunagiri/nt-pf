f5fb1af47cc0d1ecc19d146a855c57598c6441d7
NikhilTirunagiri <nikhiltirunagiri@outlook.com>
2026-01-04 15:47:47 -0500

    contact page contect

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -2,10 +2,17 @@
 
 import Sidebar from "@/components/Sidebar";
 import Topbar from "@/components/Topbar";
+import { h1, li } from "framer-motion/client";
 
 
 export default function Home(){
     return(
-        <span>Contact page</span>
+        <div>
+            <ul>
+                <li>email: <a href="mailto:ntirunag@gmu.edu" className="underline text-blue-600">ntirunag@gmu.edu</a></li>
+                <li>github: <a href="https://github.com/NikhilTirunagiri" className="underline text-blue-600">https://github.com/NikhilTirunagiri</a></li>
+                <li>linkedin: <a href="https://www.linkedin.com/in/nikhiltirunagiri/" className="underline text-blue-600">https://www.linkedin.com/in/nikhiltirunagiri/</a></li>
+            </ul>
+        </div>
     );
 }
