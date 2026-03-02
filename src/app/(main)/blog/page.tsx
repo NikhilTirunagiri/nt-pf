24c2d60c949445bd8ee80028e66ae151ee9ccbce
NikhilTirunagiri <nikhiltirunagiri@outlook.com>
2026-03-01 20:54:42 -0500

    i have no idea what change this is! lol

--- src/app/(main)/blog/page.tsx
+++ src/app/(main)/blog/page.tsx
@@ -4,15 +4,15 @@ import Sidebar from "@/components/Sidebar";
 import Topbar from "@/components/Topbar";
 
 
-export default function Home(){
-    return(
-        <div>
-            <h1 className="text-3xl font-bold pb-4">Blogs</h1>
-            <ul>
-                <li>
-                    1. <a href="/blog/AgentsInSimulation" className="underline text-blue-600">Agents in real-world simulations</a>
-                </li>
-            </ul>
-        </div>
-    );
+export default function Home() {
+	return (
+		<div>
+			<h1 className="text-3xl font-bold pb-4">Blogs</h1>
+			<ul>
+				<li>
+					1. <a href="/blog/AgentsInSimulation" className="underline text-blue-600">Agents in real-world simulations</a>
+				</li>
+			</ul>
+		</div>
+	);
 }
